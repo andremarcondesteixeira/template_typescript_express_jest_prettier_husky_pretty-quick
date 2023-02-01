09437fc19ba325e91df6e1eff3accbb43804736c
André Teixeira <andremarcondesteixeira@gmail.com>
2023-02-01 17:48:32 -0300

    moves test file to another folder

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+describe("ts-jest configuration", () => {
+    test("it should work", () => {
+        expect(true).toBe(true);
+    });
+});
--- test/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('ts-jest configuration', () => {
-    test('it should work', () => {
-        expect(true).toBe(true);
-    });
-});
